3495e96342b8e0c7e6a43c69476f8627ffc0ffad
fcoverdugo <fcoverdugoa@underpost.net>
2022-11-17 10:31:59 -0300

    update

--- app/src/main/java/com/example/francisco_verdugo/MainActivity.java
+++ app/src/main/java/com/example/francisco_verdugo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.francisco_verdugo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+               // Log.e(String, String) error
+               // Log.w(String, String) advertise
+               // Log.i(String, String) information
+               // Log.d(String, String) deputation
+               // Log.v(String, String) registry detailed
+
+        Button buttonMainActivity = (Button) findViewById(R.id.menu_btn_activity_main);
+        buttonMainActivity.setOnClickListener(v -> {
+            // Do something in response to button click}
+            Log.i("INFO", "buttonMainActivity.setOnClickListener");
+        });
+
+        Button buttonMainActivity0 = (Button) findViewById(R.id.menu_btn_activity_main_0);
+        buttonMainActivity0.setOnClickListener(v -> {
+            // Do something in response to button click}
+            Log.i("INFO", "buttonMainActivity0.setOnClickListener");
+        });
+
     }
 }
