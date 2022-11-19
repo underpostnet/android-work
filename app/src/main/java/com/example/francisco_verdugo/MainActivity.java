2be4e1efa64ca4560780754b82eac2a0c93344bf
fcoverdugo <fcoverdugoa@underpost.net>
2022-11-19 18:34:08 -0300

    update

--- app/src/main/java/com/example/francisco_verdugo/MainActivity.java
+++ app/src/main/java/com/example/francisco_verdugo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.francisco_verdugo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -12,12 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ComponentMenu menuInstance = new ComponentMenu();
+        Log.i("INFO", "onCreate MainActivity");
+        // ComponentMenu menuInstance = new ComponentMenu();
 
         Button buttonMainActivity = (Button) findViewById(R.id.menu_btn_activity_main);
         Button buttonMainActivity0 = (Button) findViewById(R.id.menu_btn_activity_main_0);
-        
-        menuInstance.initBtnMenuEvent(buttonMainActivity, buttonMainActivity0);
+
+        buttonMainActivity.setOnClickListener(v -> {
+            // Do something in response to button click}
+            Log.i("INFO", "buttonMainActivity.setOnClickListener");
+        });
+
+        buttonMainActivity0.setOnClickListener(v -> {
+            // Do something in response to button click}
+            Log.i("INFO", "buttonMainActivity0.setOnClickListener");
+            Intent intent = new Intent(MainActivity.this, MainActivity0.class);
+            startActivity(intent);
+        });
+
+        // menuInstance.initBtnMenuEvent(buttonMainActivity, buttonMainActivity0);
 
     }
 }
--- app/src/main/java/com/example/francisco_verdugo/MainActivity0.java
+++ app/src/main/java/com/example/francisco_verdugo/MainActivity0.java
@@ -2,7 +2,10 @@ package com.example.francisco_verdugo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity0 extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity0 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main0);
+        Log.i("INFO", "onCreate MainActivity0");
+
+        Button buttonMainActivity = (Button) findViewById(R.id.menu_btn_activity_main);
+        Button buttonMainActivity0 = (Button) findViewById(R.id.menu_btn_activity_main_0);
+
+        buttonMainActivity.setOnClickListener(v -> {
+            // Do something in response to button click}
+            Log.i("INFO", "buttonMainActivity.setOnClickListener");
+            Intent intent = new Intent(MainActivity0.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        buttonMainActivity0.setOnClickListener(v -> {
+            // Do something in response to button click}
+            Log.i("INFO", "buttonMainActivity0.setOnClickListener");
+        });
     }
 }
