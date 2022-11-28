825becec2aaf0866dc49a60c0e04676457558e7f
fcoverdugo <fcoverdugoa@underpost.net>
2022-11-28 16:58:16 -0300

    update

--- app/src/main/java/com/example/francisco_verdugo/CaptureActivity.java
+++ app/src/main/java/com/example/francisco_verdugo/CaptureActivity.java
@@ -2,13 +2,35 @@ package com.example.francisco_verdugo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CaptureActivity extends AppCompatActivity {
+
+    public ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
+
+        Button button = (Button) findViewById(R.id.submit_btn_capture_cam);
+        imageView = (ImageView) findViewById(R.id.view_capture_cam);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 0);
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
