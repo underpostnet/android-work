ffb8df198f397289f23bc8c47854eea3d9e3a54e
fcoverdugo <fcoverdugoa@underpost.net>
2022-12-05 13:57:07 -0300

    t61

--- app/src/main/java/com/example/francisco_verdugo/MainActivity.java
+++ app/src/main/java/com/example/francisco_verdugo/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.francisco_verdugo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// import android.content.Intent;
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Button;
+// import android.view.Menu;
+// import android.view.MenuInflater;
+// import android.view.MenuItem;
+// import android.widget.Button;
 import android.widget.ListView;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import java.util.ArrayList;
 
@@ -27,28 +38,28 @@ public class MainActivity extends AppCompatActivity {
         Log.i("INFO", "onCreate MainActivity");
 
 
-        Button menu_adm_capture = findViewById(R.id.menu_adm_capture);
-        Button menu_adm_list = findViewById(R.id.menu_adm_list);
-        Button menu_adm_gfx = findViewById(R.id.menu_adm_gfx);
+        // Button menu_adm_capture = findViewById(R.id.menu_adm_capture);
+        // Button menu_adm_list = findViewById(R.id.menu_adm_list);
+        // Button menu_adm_gfx = findViewById(R.id.menu_adm_gfx);
 
-        menu_adm_capture.setOnClickListener(v -> {
-            // Do something in response to button click}
-            Log.i("INFO", "menu_adm_capture.setOnClickListener");
-            Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
-            startActivity(intent);
-        });
-        menu_adm_list.setOnClickListener(v -> {
-            // Do something in response to button click}
-            Log.i("INFO", "menu_adm_list.setOnClickListener");
-            Intent intent = new Intent(MainActivity.this, ListActivity.class);
-            startActivity(intent);
-        });
-        menu_adm_gfx.setOnClickListener(v -> {
-            // Do something in response to button click}
-            Log.i("INFO", "menu_adm_gfx.setOnClickListener");
-            Intent intent = new Intent(MainActivity.this, GfxActivity.class);
-            startActivity(intent);
-        });
+        // menu_adm_capture.setOnClickListener(v -> {
+        //     // Do something in response to button click}
+        //     Log.i("INFO", "menu_adm_capture.setOnClickListener");
+        //     Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+        //     startActivity(intent);
+        // });
+        // menu_adm_list.setOnClickListener(v -> {
+        //     // Do something in response to button click}
+        //     Log.i("INFO", "menu_adm_list.setOnClickListener");
+        //     Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        //     startActivity(intent);
+        // });
+        // menu_adm_gfx.setOnClickListener(v -> {
+        //     // Do something in response to button click}
+        //     Log.i("INFO", "menu_adm_gfx.setOnClickListener");
+        //     Intent intent = new Intent(MainActivity.this, GfxActivity.class);
+        //     startActivity(intent);
+        // });
 
         // this.renderList();
         // this.renderListAdapter();
@@ -73,6 +84,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+    }
+    public void onClickAddDetails(View view) {
+        ContentValues values = new ContentValues();
+        values.put(DataFlairProvider.name, ((EditText)        findViewById(R.id.txtName)).getText().toString());
+        getContentResolver().insert(DataFlairProvider.CONTENT_URI, values);
+        Toast.makeText(getBaseContext(), "New Record Inserted", Toast.LENGTH_LONG).show();
+    }
+
+    @SuppressLint("Range")
+    public void onClickShowDetails(View view) {
+        TextView resultView= (TextView) findViewById(R.id.res);
+        Cursor cursor = getContentResolver().query(Uri.parse("content://com.dataflair.contentprovider.UserProvider/users"), null, null, null, null);
+        if(cursor.moveToFirst()) {
+            StringBuilder strBuild=new StringBuilder();
+            while (!cursor.isAfterLast()) {
+                strBuild.append("\n"+cursor.getString(cursor.getColumnIndex("id"))+ "-"+ cursor.getString(cursor.getColumnIndex("name")));
+                cursor.moveToNext();
+            }
+            resultView.setText(strBuild);
+        }
+        else {
+            resultView.setText("No Records Found");
+        }
+    }
+
     // private void renderList() {
     //     this.listview = findViewById(R.id.formula1_list_view);
     //     this.names.add("Mercedes");
@@ -84,29 +125,29 @@ public class MainActivity extends AppCompatActivity {
     // }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        Log.i("INFO", "onCreateOptionsMenu");
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
+    // @Override
+    // public boolean onCreateOptionsMenu(Menu menu) {
+    //     Log.i("INFO", "onCreateOptionsMenu");
+    //     MenuInflater inflater = getMenuInflater();
+    //     inflater.inflate(R.menu.menu, menu);
+    //     return true;
+    // }
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.add_item_formula1_list:
-                Log.i("INFO", "add_item_formula1_list");
-                Intent intent = new Intent(MainActivity.this, MainActivity0.class);
-                startActivity(intent);
-                return true;
-            case 0:
-                return false;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+    // @SuppressLint("NonConstantResourceId")
+    // @Override
+    // public boolean onOptionsItemSelected(MenuItem item) {
+    //     // Handle item selection
+    //     switch (item.getItemId()) {
+    //         case R.id.add_item_formula1_list:
+    //             Log.i("INFO", "add_item_formula1_list");
+    //             Intent intent = new Intent(MainActivity.this, MainActivity0.class);
+    //             startActivity(intent);
+    //             return true;
+    //         case 0:
+    //             return false;
+    //         default:
+    //             return super.onOptionsItemSelected(item);
+    //     }
+    // }
 
 }
