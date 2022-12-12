75b3b8be2264cf79fa76809f93eda9a71aac95fe
fcoverdugo <fcoverdugoa@underpost.net>
2022-12-12 19:29:38 -0300

    update

--- app/src/main/java/com/example/francisco_verdugo/LogInActivity.java
+++ app/src/main/java/com/example/francisco_verdugo/LogInActivity.java
@@ -4,11 +4,79 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
 public class LogInActivity extends AppCompatActivity {
 
+    SharedPreferences sharedpreferences;
+    TextView name;
+    TextView email;
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Email = "emailKey";
+
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        name = (TextView) findViewById(R.id.etPassword);
+        email = (TextView) findViewById(R.id.etEmail);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+
+    }
+
+    public void Save(View view) {
+        String n = name.getText().toString();
+        String e = email.getText().toString();
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(Name, n);
+        editor.putString(Email, e);
+        editor.commit();
+    }
+
+    public void clear(View view) {
+        name = (TextView) findViewById(R.id.etPassword);
+        email = (TextView) findViewById(R.id.etEmail);
+        name.setText("");
+        email.setText("");
+
+    }
+
+    public void Get(View view) {
+        name = (TextView) findViewById(R.id.etPassword);
+        email = (TextView) findViewById(R.id.etEmail);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+
+        if (sharedpreferences.contains(Name)) {
+            name.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Email)) {
+            email.setText(sharedpreferences.getString(Email, ""));
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.i("INFO", "click onCreateOptionsMenu");
+        return true;
     }
 }
